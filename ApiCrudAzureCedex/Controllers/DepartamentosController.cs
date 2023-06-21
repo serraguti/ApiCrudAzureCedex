d39016eb18c88193a20f9b1f8f3fe688a6e71cb4
serraguti <pacoserranox@gmail.com>
2023-06-21 10:49:23 +0200

    commit

--- ApiCrudAzureCedex/Controllers/DepartamentosController.cs
+++ ApiCrudAzureCedex/Controllers/DepartamentosController.cs
@@ -29,5 +29,27 @@ namespace ApiCrudAzureCedex.Controllers
         {
             return await this.repo.FindDepartamentoAsync(id);
         }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateDepartamento(Departamento departamento)
+        {
+            await this.repo.Create(departamento.Nombre, departamento.Localidad);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateDepartamento(Departamento departamento)
+        {
+            await this.repo.Update(departamento.IdDepartamento
+                , departamento.Nombre, departamento.Localidad);
+            return Ok(new { mensaje = "Update correcto" });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteDepartamento(int id)
+        {
+            await this.repo.Delete(id);
+            return Ok();
+        }
     }
 }
--- ApiCrudAzureCedex/Repositories/RepositoryHospital.cs
+++ ApiCrudAzureCedex/Repositories/RepositoryHospital.cs
@@ -23,5 +23,42 @@ namespace ApiCrudAzureCedex.Repositories
             return await this.context.Departamentos
                 .FirstOrDefaultAsync(x => x.IdDepartamento == id);
         }
+
+        private async Task<int> GetMaxIdDepartamentoAsync()
+        {
+            if (this.context.Departamentos.Count() == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return await this.context.Departamentos.MaxAsync(x => x.IdDepartamento) + 1;
+            }
+        }
+
+        public async Task Create(string nombre, string localidad)
+        {
+            Departamento dept = new Departamento();
+            dept.IdDepartamento = await this.GetMaxIdDepartamentoAsync();
+            dept.Nombre = nombre;
+            dept.Localidad = localidad;
+            this.context.Departamentos.Add(dept);
+            await this.context.SaveChangesAsync();
+        }
+
+        public async Task Update(int id, string nombre, string localidad)
+        {
+            Departamento dept = await this.FindDepartamentoAsync(id);
+            dept.Nombre = nombre;
+            dept.Localidad = localidad;
+            await this.context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            Departamento dept = await this.FindDepartamentoAsync(id);
+            this.context.Departamentos.Remove(dept);
+            await this.context.SaveChangesAsync();
+        }
     }
 }

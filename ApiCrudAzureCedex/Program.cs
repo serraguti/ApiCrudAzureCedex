619114b6a21073f19b1243c0ff783988680f741e
serraguti <pacoserranox@gmail.com>
2023-06-21 11:30:11 +0200

    commit

--- ApiCrudAzureCedex/Program.cs
+++ ApiCrudAzureCedex/Program.cs
@@ -1,6 +1,7 @@
 using ApiCrudAzureCedex.Data;
 using ApiCrudAzureCedex.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,15 +15,32 @@ builder.Services.AddDbContext<HospitalContext>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//DEBEMOS PERSONALIZAR/DOCUMENTAR QUIENES SOMOS EN SWAGGER
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Api Azure Crud Cedex",
+        Description = "Estamos en clase de Azure y no funciona Azure",
+        Version = "v1",
+        Contact = new OpenApiContact()
+        {
+             Name = "Paco Garcia Serrano", Email = "pacoserranox@gmail.com"
+        }
+    });
+});
 
 var app = builder.Build();
-
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "Api V1 Azure");
+    options.RoutePrefix = "";
+});
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
 }
 
 app.UseHttpsRedirection();
